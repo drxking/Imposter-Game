10f373ca0676b6f76c1e6e1978310658f48c9e50
Sudip Acharya <drxking456@gmail.com>
2026-02-24 22:23:24 +0545

    Game Done

---
+++ src/main.js
@@ -0,0 +1,170 @@
+import data from "../src/data.json"
+
+let playerCount = 4;
+const maxPlayer = 16;
+const minPlayer = 3;
+let settingStatus = 0;
+
+
+let gameData = [...data['GOT']]
+
+let playerCountBox = document.getElementById("playerCountBox");
+let addButton = document.getElementById("add")
+let subButton = document.getElementById("sub")
+let settingButton = document.getElementById("setting-hide")
+let settingBox = document.getElementById("setting");
+let saveAndRestartButton = document.getElementById("save-button")
+let startButton = document.getElementById("start-button")
+let wordBox = document.getElementById("wordBox")
+let playerBox = document.getElementById("player-box")
+
+function generateRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+addButton.addEventListener("click", increment)
+subButton.addEventListener("click", decrement)
+saveAndRestartButton.addEventListener("click", saveAndRestart)
+startButton.addEventListener("click", buttonClick)
+
+settingButton.addEventListener("click", () => {
+    console.log("Hello")
+    if (settingStatus) {
+        settingBox.classList.add("hidden")
+        settingStatus = 0;
+
+    } else {
+        settingStatus = 1
+        settingBox.classList.remove("hidden")
+    }
+})
+
+function increment() {
+    subButton.style.cursor = "pointer";
+    subButton.style.opacity = 1;
+    if (playerCount == maxPlayer) {
+        return;
+    }
+    playerCount++;
+    if (playerCount == maxPlayer) {
+        addButton.style.cursor = 'not-allowed';
+        addButton.style.opacity = 0.5;
+    }
+    render()
+}
+
+function decrement() {
+    addButton.style.cursor = "pointer"
+    addButton.style.opacity = 1;
+    if (playerCount == minPlayer) {
+        return;
+    }
+    playerCount--;
+    if (playerCount == minPlayer) {
+        subButton.style.cursor = "not-allowed"
+        subButton.style.opacity = 0.5;
+    }
+    render();
+}
+
+
+
+function render() {
+    playerCountBox.innerText = playerCount
+}
+
+let word = "";
+let imposter = 0;
+let currentViewer = 1;
+let inView = false;
+
+
+function buttonClick() {
+    if (!inView) {
+        console.log(currentViewer)
+        console.log("CLicked")
+        if (currentViewer == 1) {
+            startGame()
+            startButton.innerText = "Hide and Pass"
+        }
+        else if (currentViewer == playerCount) {
+            wordRender();
+            startButton.innerText = "Hide and Exit"
+        }
+        else if (currentViewer == playerCount + 1) {
+            startButton.innerText = "Reveal Imposter";
+            currentViewer++;
+        } else if (currentViewer == playerCount + 2) {
+            playerBox.innerText = `Player ${imposter} is `
+            wordBox.innerText = `Imposter`
+            startButton.innerText = "Restart Game"
+            currentViewer++;
+
+        }
+        else if (currentViewer == playerCount + 3) {
+            endGame()
+            console.log("REsart")
+            // inView = true;
+        }
+        else {
+            wordRender();
+            startButton.innerText = "Hide and Pass"
+        }
+    } else {
+
+        if (currentViewer == playerCount + 1) {
+            playerBox.innerText = `Guess The Word`;
+            wordBox.innerText = "Guess The Imposter";
+            startButton.innerText = "Reveal Imposter";
+        } else {
+            playerBox.innerText = `Player ${currentViewer}`;
+            wordBox.innerText = "No one Knows!!!";
+            startButton.innerText = "Reveal Word";
+        }
+
+        inView = false
+    }
+}
+
+
+
+function startGame() {
+    word = gameData[generateRandom(0, gameData.length - 1)];
+    imposter = generateRandom(1, playerCount);
+    console.log(word)
+    wordRender()
+}
+
+function wordRender() {
+    playerBox.innerText = `Player ${currentViewer}`;
+    wordBox.innerText = currentViewer === imposter ? "Imposter" : word;
+    currentViewer++;
+    inView = true;
+}
+
+function endGame() {
+    playerBox.innerText = `Ready?`;
+    wordBox.innerText = "Game of Thrones Edition";
+    startButton.innerText = "Show my Word";
+    word = "";
+    imposter = 0;
+    currentViewer = 1;
+    inView = false;
+}
+
+
+function saveAndRestart() {
+    console.log("Saved");
+    gameData = [];
+    document.querySelectorAll(".series-checkbox").forEach((e) => {
+        if (e.checked) {
+            console.log(e.value)
+            gameData = [...gameData, ...data[e.value]]
+
+        }
+    })
+    console.log(gameData)
+    settingBox.classList.add("hidden")
+    settingStatus = 0;
+    endGame();
+}
